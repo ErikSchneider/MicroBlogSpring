dacd873b7607b70ad60d7d1c22b2edebf61d6d06
Erik <eschneider1128@gmail.com>
2016-06-20 15:36:48 -0400

    removed user class. fixed add message and added delete

--- src/main/java/com/theironyard/MicroBlogSpringController.java
+++ src/main/java/com/theironyard/MicroBlogSpringController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -18,13 +19,15 @@ public class MicroBlogSpringController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home (Model model, HttpSession session) {
-
         String username = (String) session.getAttribute("username");
-        User user = null;
-        if (username != null) {
-            user = new User(username);
+        model.addAttribute("username", username);
+        Integer id = 1;
+        for (Message message : messages) {
+            message.id = id;
+            id++;
         }
-        model.addAttribute("user", user);
+        model.addAttribute("messages", messages);
+
 
         return "home";
     }
@@ -34,9 +37,15 @@ public class MicroBlogSpringController {
         return "redirect:/";
     }
     @RequestMapping(path = "/add-message", method = RequestMethod.POST)
-    public String addMessage (String text) {
-        messages.add(new Message(text));
-
+    public String addMessage (String message) {
+        Message msg = new Message(message);
+        messages.add(msg);
+        return "redirect:/";
+    }
+    @RequestMapping(path = "/delete-message", method = RequestMethod.POST)
+    public String deleteMessage (Integer id) {
+        messages.remove(id - 1);
+        return "redirect:/";
     }
 
     @RequestMapping(path = "/logout", method = RequestMethod.POST)
